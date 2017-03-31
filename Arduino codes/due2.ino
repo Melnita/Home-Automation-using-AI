198f7ca6a69fbea15d9dc9b47a94a4e319b7d426
Melnita <melnita116@hotmail.com>
2017-03-31 23:40:47 +0530

    Due2 proper code

--- Arduino codes/due2.ino
+++ Arduino codes/due2.ino
@@ -1,21 +1,19 @@
+#define RELAYBULB 7
+
 void setup() {
   // put your setup code here, to run once:
-pinMode(7,OUTPUT);
-pinMode(13,OUTPUT);
-//Serial.begin(9600);
+pinMode(RELAYBULB,OUTPUT);
+Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- // if(Serial.available()){
-//  char re=Serial.read();
-   // digitalWrite(13, LOW);
- // if(re=='a'){ 
-    digitalWrite(7, HIGH);
- // } 
-    delay(2000);
-  //if(re=='c'){ 
-    digitalWrite(7, LOW);
-    delay(2000);
-  //}
+  if(Serial.available()){
+  char re=Serial.read();
+  if(re=='a'){ 
+    digitalWrite(RELAYBULB, HIGH);
+  } 
+  if(re=='c'){ 
+    digitalWrite(RELAYBULB, LOW);
+  }
 }
